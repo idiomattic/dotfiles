26cb16b5d503fc1a0abf85d93ed72598f445d9e8
Matthew Lese <matthewjlese@gmail.com>
2024-12-10 10:23:34 -0800

    injects SEARCH_RANK_MAC_CHEESE_VARIANT in shell

--- .private/1p-secrets.sh
+++ .private/1p-secrets.sh
@@ -15,7 +15,7 @@ export NPM_TOKEN="{{ op://Employee/5wwxlemqpc45d5xc3uppztms7y/password }}"
 # Misc secrets
 export SEARCH_VIS_API_KEY_PROD="{{ op://Employee/SEARCH_VIS_API_KEY_PROD/password }}"
 export SEARCH_VIS_API_KEY="{{ op://Employee/SEARCH_VIS_API_KEY/password }}"
-export SEARCH_RANK_V3_TEST_VARIANT="{{ op://Employee/SEARCH_RANK_V3_TEST_VARIANT/password }}"
+export SEARCH_RANK_MAC_CHEESE_VARIANT="{{ op://Employee/SEARCH_RANK_MAC_CHEESE_VARIANT/password }}"
 export SEARCH_RANK_LASAGNA_VARIANT="{{ op://Employee/SEARCH_RANK_LASAGNA_VARIANT/password }}"
 export SEARCH_ILB_IP="{{ op://Employee/SEARCH_ILB_IP/URL }}"
 export SEARCH_ILB_IP_DATA_ES="{{ op://Employee/SEARCH_ILB_IP_DATA_ES/URL }}"
